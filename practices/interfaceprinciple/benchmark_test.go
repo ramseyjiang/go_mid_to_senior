c1c4936a60c27c3dcf4666a17d525a376dc42eb1
ramseyjiang <ramseyjiang@gmail.com>
2022-05-08 11:18:56 +1200

    update benchmark name

--- practices/interfaceprinciple/benchmark_test.go
+++ practices/interfaceprinciple/benchmark_test.go
@@ -6,14 +6,14 @@ var efaceKnowType int
 var efaceUnknownType uint32
 
 /**
-Benchmark_EfaceKnowType
-Benchmark_EfaceKnowType/eface-to-type
-Benchmark_EfaceKnowType/eface-to-type-10         	1000000000	         0.4796 ns/op
-Benchmark_EfaceKnowType/int-to-int
-Benchmark_EfaceKnowType/int-to-int-10            	1000000000	         0.3163 ns/op
+BenchmarkEfaceKnowType
+BenchmarkEfaceKnowType/eface-to-type
+BenchmarkEfaceKnowType/eface-to-type-10         	1000000000	         0.4796 ns/op
+BenchmarkEfaceKnowType/int-to-int
+BenchmarkEfaceKnowType/int-to-int-10            	1000000000	         0.3163 ns/op
 */
 // The result above is used to test know types result.
-func Benchmark_EfaceKnowType(b *testing.B) {
+func BenchmarkEfaceKnowType(b *testing.B) {
 	b.Run("eface-to-type", func(b *testing.B) {
 		var ebread interface{} = 666
 		for i := 0; i < b.N; i++ {
@@ -30,14 +30,14 @@ func Benchmark_EfaceKnowType(b *testing.B) {
 }
 
 /**
-Benchmark_EfaceUnknownType
-Benchmark_EfaceUnknownType/switch-small
-Benchmark_EfaceUnknownType/switch-small-10         	756420516	         1.587 ns/op
-Benchmark_EfaceUnknownType/switch-big
-Benchmark_EfaceUnknownType/switch-big-10           	545435434	         2.197 ns/op
+BenchmarkEfaceUnknownType
+BenchmarkEfaceUnknownType/switch-small
+BenchmarkEfaceUnknownType/switch-small-10         	756420516	         1.587 ns/op
+BenchmarkEfaceUnknownType/switch-big
+BenchmarkEfaceUnknownType/switch-big-10           	545435434	         2.197 ns/op
 */
 // The result above is used to test iface unknown types result.
-func Benchmark_EfaceUnknownType(b *testing.B) {
+func BenchmarkEfaceUnknownType(b *testing.B) {
 	var ebread interface{} = uint32(42)
 
 	b.Run("switch-small", func(b *testing.B) {
