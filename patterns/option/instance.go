41f7a70ba99973e86e69fa79e0759eba78b0c6bc
ramseyjiang <ramseyjiang@gmail.com>
2022-05-29 16:33:47 +1200

    add option pattern

---
+++ patterns/option/instance.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+type Server struct {
+	port    string
+	timeout time.Duration
+	maxConn int64
+}
+
+// Option is the keypoint for the option pattern. It is used to define a type of function which is optional.
+type Option func(*Server)
+
+func NewServer(port string, options ...Option) *Server {
+	server := &Server{
+		port: port,
+	}
+
+	for _, option := range options {
+		option(server)
+	}
+
+	return server
+}
+
+func WithTimeout(timeout time.Duration) Option {
+	return func(s *Server) {
+		s.timeout = timeout
+	}
+}
+
+func WithMaxConn(maxConn int64) Option {
+	return func(s *Server) {
+		s.maxConn = maxConn
+	}
+}
+
+func main() {
+	server1 := NewServer("4000")
+	log.Println(server1)
+
+	server2 := NewServer("5000", WithTimeout(2*time.Second))
+	log.Println(server2)
+
+	server3 := NewServer("6000", WithMaxConn(100))
+	log.Println(server3)
+}
---
+++ patterns/option/readme.md
@@ -0,0 +1,7 @@
+**What's the Option pattern?**
+Option pattern is used to provide optional arguments to a function that can be used to modify its behavior.
+
+In Golang, it does not allow to set a default value for a function parameter. So this way, it is a good example. 
+But if you want to use this way, you should consider your scenario, because this way adds more work for code and not good maintainable.
+
+Perhaps it can be used as types convert function.
