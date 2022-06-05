5def5ec9cb6f2c0f4f5197ff1818ac31085e49d2
ramseyjiang <ramseyjiang@gmail.com>
2022-06-05 17:52:18 +1200

    update customise error pkg

--- practices/noneresource/customiseerror.go
+++ practices/noneresource/customiseerror.go
@@ -1,17 +1,17 @@
-package practices
+package main
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/ramseyjiang/go_mid_to_senior/customizepkgs/goerr"
 )
 
-func TriggerErrorPkg() {
-	fmt.Println(ResourceNotFound("1234", "message", "User", nil).Error())
+func main() {
+	log.Println(ResourceNotFound("1234", "message", "User", nil).Error())
 }
 
 // ResourceNotFound error abstraction
-func ResourceNotFound(id, message string, kind string, cause error) goerr.Error {
+func ResourceNotFound(id string, message string, kind string, cause error) goerr.Error {
 	data := map[string]interface{}{"kind": kind, "id": id}
 	return goerr.NewGoError("ResourceNotFound", message, data, cause).
 		SetComponent(goerr.ErrService).SetResponseType(goerr.NotFound)
