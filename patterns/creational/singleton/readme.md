9e2ee2f6c6fb0e64b89bc871b23b7fe2b9191cde
ramseyjiang <ramseyjiang@gmail.com>
2022-09-26 20:57:05 +1300

    update singleton and its unittest

---
+++ patterns/creational/singleton/readme.md
@@ -0,0 +1,5 @@
+Acceptance test criteria:
+
+a. When no counter has been created before, a new one is created with the value 0. b. If a counter has already been
+created, return this instance that holds the actual count. c. If we call the method AddOne, the count must be
+incremented by 1.
---
+++ patterns/creational/singleton/singleton_test.go
@@ -0,0 +1,28 @@
+package singleton
+
+import "testing"
+
+func TestGetInstance(t *testing.T) {
+	counter1 := GetInstance()
+	if counter1 == nil {
+		// Test of acceptance criteria 1 failed
+		t.Error("expected pointer to Singleton after calling GetInstance(), not nil")
+	}
+
+	currentCount := counter1.AddOne()
+	if currentCount != 1 {
+		t.Errorf("After calling for the first time to count, the count must be 1 but it is %d\n", currentCount)
+	}
+
+	expectedCounter := counter1
+	counter2 := GetInstance()
+	if counter2 != expectedCounter {
+		// Test 2 failed
+		t.Error("Expected same instance in counter2 but it got a different instance")
+	}
+
+	currentCount = counter2.AddOne()
+	if currentCount != 2 {
+		t.Errorf("After calling 'AddOne' using the second counter, the current count must be 2 but was %d\n", currentCount)
+	}
+}
