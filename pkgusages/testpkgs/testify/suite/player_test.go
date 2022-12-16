6e7ea227d3fce74e8610f5a7f496bac8fea1322e
ramseyjiang <ramseyjiang@gmail.com>
2022-12-16 16:34:03 +1300

    update tests

--- pkgusages/testpkgs/testify/suite/player_test.go
+++ pkgusages/testpkgs/testify/suite/player_test.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/stretchr/testify/suite"
 )
 
 type GameTestSuite struct {
 	suite.Suite
+}
+
+func (suite *GameTestSuite) SetupSuite() {
+	// create relevant resources
+	fmt.Println("SETUP")
 }
 
 func (suite *GameTestSuite) BeforeTest(_, _ string) {
@@ -21,46 +28,54 @@ func (suite *GameTestSuite) AfterTest(_, _ string) {
 	fmt.Println("AFTER")
 }
 
-func (suite *GameTestSuite) SetupSuite() {
-	// create relevant resources
-	fmt.Println("SETUP")
-}
-
 func TestGameTestSuite(t *testing.T) {
 	suite.Run(t, new(GameTestSuite))
 }
 
+// notice: suite.Require().Equal() usage is different from assert.Equal().
 func (suite *GameTestSuite) TestHadAGoodGame() {
 	tests := []struct {
-		name     string
-		stats    Stats
-		goodGame bool
-		wantErr  string
+		name          string
+		stats         Stats
+		goodGame      bool
+		errorExpected bool
+		errorMessage  string
 	}{
-		{"sad path: invalid stats", Stats{Name: "Sam Cassell",
-			Minutes:   34.1,
-			Points:    -19,
-			Assists:   8,
-			Turnovers: -4,
-			Rebounds:  11,
-		}, false, "stat lines cannot be negative",
+		{"sad path: invalid stats",
+			Stats{Name: "Sam Cassell",
+				Minutes:   34.1,
+				Points:    -19,
+				Assists:   8,
+				Turnovers: -4,
+				Rebounds:  11,
+			},
+			false,
+			true,
+			"stat lines cannot be negative",
 		},
-		{"happy path: good game", Stats{Name: "Dejounte Murray",
-			Minutes:   34.1,
-			Points:    19,
-			Assists:   8,
-			Turnovers: 4,
-			Rebounds:  11,
-		}, true, ""},
+		{"happy path: good game",
+			Stats{Name: "Dejounte Murray",
+				Minutes:   34.1,
+				Points:    19,
+				Assists:   8,
+				Turnovers: 4,
+				Rebounds:  11,
+			},
+			true,
+			false,
+			""},
 	}
 	for _, tt := range tests {
 		suite.T().Run("test hadAGoodGame(): "+tt.name, func(t *testing.T) {
+			// Act
 			isAGoodGame, err := hadAGoodGame(tt.stats)
-			if tt.wantErr != "" {
-				suite.Require().Contains(err.Error(), tt.wantErr)
-			} else {
-				suite.Require().Equal(tt.goodGame, isAGoodGame)
+
+			if tt.errorExpected {
+				// Assert
+				require.Error(t, err)
+				suite.Require().Equal(tt.errorMessage, err.Error())
 			}
+			suite.Require().Equal(tt.goodGame, isAGoodGame)
 		})
 	}
 }
