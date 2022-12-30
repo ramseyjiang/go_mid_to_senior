f24d6f2be2ee711efbf3b92528786ad2a3a9d267
ramseyjiang <ramseyjiang@gmail.com>
2022-12-30 23:07:32 +1300

    update

--- pkgusages/signal/main.go
+++ pkgusages/signal/main.go
@@ -2,43 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
+var conf = &Config{Message: "Hello, World!"}
+
+type Config struct {
+	Message string
+}
+
 func multiSignalHandler(signal os.Signal) {
 	switch signal {
 	case syscall.SIGHUP:
 		fmt.Println("Signal:", signal.String())
-		os.Exit(0)
+		log.Println("hot reload")
+		conf.Message = "Go To Hell!"
 	case syscall.SIGINT:
 		fmt.Println("Signal:", signal.String())
-		os.Exit(0)
 	case syscall.SIGTERM:
 		fmt.Println("Signal:", signal.String())
-		os.Exit(0)
 	case syscall.SIGQUIT:
 		fmt.Println("Signal:", signal.String())
-		os.Exit(0)
 	default:
 		fmt.Println("Unhandled/unknown signal")
 	}
 }
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(conf.Message))
+	})
+
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
-	exitCh := make(chan int)
 
-	fmt.Println("start")
 	go func() {
-		for {
-			s := <-sigCh
-			multiSignalHandler(s)
-		}
+		log.Println("starting up....")
+		log.Fatal(http.ListenAndServe(":8080", nil))
 	}()
 
-	exitCode := <-exitCh
-	os.Exit(exitCode)
+	for {
+		s := <-sigCh
+		multiSignalHandler(s)
+	}
+
+	// for {
+	// 	select {
+	// 	case <-sigCh:
+	// 		log.Println("hot reload")
+	// 		conf.Message = "Go To Hell!"
+	// 	}
+	// }
 }
--- pkgusages/signal/readme.md
+++ pkgusages/signal/readme.md
@@ -1,9 +1,18 @@
 1. open a terminal, % go run main.go
 
-2. open the other teminal % ps -e
+2. open the second terminal, try to find the main.go processID % ps -e
 
-3. In the second terminal, try to find the main.go processID
-4. In the second terminal % kill -SIGHUP processID
+3. In the second terminal, make the main.go run in the background % kill -SIGHUP processID
 
-5. check the first terminal, you will see the main.go has stopped, and it will show "[1] processID hangup go run
-   main.go"
+check the first terminal, you will see the main.go has stopped, and it will show "[1] processID hangup go run main.go"
+
+**4. Find current port-number whether occupy and return the port processID**
+% lsof -i tcp:port-number
+
+5. Kill the processID bound with port-number % kill processID // in the first terminal, it will show "[1] 81676
+   terminated go run main.go"
+   or % kill -SIGHUP processID     
+   // it will show // "[1] processID hangup go run main.go"
+   // "2022/12/30 23:02:43 hot reload"
+
+6. Kill the port-number processID forever % kill -9 processID
