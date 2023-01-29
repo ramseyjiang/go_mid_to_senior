f2a84c24d52b49263e36f63cea0e36fa9044313f
ramseyjiang <ramseyjiang@gmail.com>
2023-01-29 22:09:32 +1300

    add comment

--- patterns/creational/singleton/singleton.go
+++ patterns/creational/singleton/singleton.go
@@ -20,6 +20,7 @@ var instance *singleton
 // GetInstance is used to return a singleton instance.
 // If instance is not nil, it means the instance has been new as a singleton.
 // If instance is nil, it means the instance has not new yet, so new it and return.
+// If commented line 26, 27, 28, 30, it also works. Here just one example for how to use once.Do().
 func GetInstance() *singleton {
 	if instance == nil {
 		once.Do(
