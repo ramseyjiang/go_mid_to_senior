0a6280f53a64de8e1f9f21935f126d2c1c0e2b60
ramseyjiang <ramseyjiang@gmail.com>
2023-02-06 20:01:57 +1300

    update

--- patterns/creational/builder/vehicle/builder.go
+++ patterns/creational/builder/vehicle/builder.go
@@ -1,18 +1,27 @@
 package vehicle
 
-type BuildProcess interface {
-	SetWheels() BuildProcess
-	SetSeats() BuildProcess
-	SetStructure() BuildProcess
-	GetVehicle() ProductVehicle
+// Builder is the interface that specifies the methods for building the parts of the product
+type Builder interface {
+	SetWheels() Builder
+	SetSeats() Builder
+	SetStructure() Builder
+	GetVehicle() Product
 }
 
+// Product represents the complex object that we want to build
+type Product struct {
+	Wheels    int
+	Seats     int
+	Structure string
+}
+
+// ManufactureDirector is responsible for using the Builder interface to build the product
 type ManufactureDirector struct {
-	builder BuildProcess
+	builder Builder
 }
 
 // SetBuilder allow to change the builder that is being used in the Manufacturing director
-func (f *ManufactureDirector) SetBuilder(b BuildProcess) {
+func (f *ManufactureDirector) SetBuilder(b Builder) {
 	f.builder = b
 }
 
@@ -21,69 +30,67 @@ func (f *ManufactureDirector) Construct() {
 	f.builder.SetSeats().SetStructure().SetWheels()
 }
 
-type ProductVehicle struct {
-	Wheels    int
-	Seats     int
-	Structure string
-}
-
+// CarBuilder is an implementation of the Builder interface
 type CarBuilder struct {
-	v ProductVehicle
+	v Product
 }
 
-func (c *CarBuilder) SetWheels() BuildProcess {
+func (c *CarBuilder) SetWheels() Builder {
 	c.v.Wheels = 4
 	return c
 }
-func (c *CarBuilder) SetSeats() BuildProcess {
+func (c *CarBuilder) SetSeats() Builder {
 	c.v.Seats = 5
 	return c
 }
-func (c *CarBuilder) SetStructure() BuildProcess {
+func (c *CarBuilder) SetStructure() Builder {
 	c.v.Structure = "Car"
 	return c
 }
-func (c *CarBuilder) GetVehicle() ProductVehicle {
+
+// GetVehicle returns the built product
+func (c *CarBuilder) GetVehicle() Product {
 	return c.v
 }
 
+// BikeBuilder is an implementation of the Builder interface
 type BikeBuilder struct {
-	v ProductVehicle
+	v Product
 }
 
-func (b *BikeBuilder) SetWheels() BuildProcess {
+func (b *BikeBuilder) SetWheels() Builder {
 	b.v.Wheels = 2
 	return b
 }
-func (b *BikeBuilder) SetSeats() BuildProcess {
+func (b *BikeBuilder) SetSeats() Builder {
 	b.v.Seats = 2
 	return b
 }
-func (b *BikeBuilder) SetStructure() BuildProcess {
+func (b *BikeBuilder) SetStructure() Builder {
 	b.v.Structure = "Bike"
 	return b
 }
-func (b *BikeBuilder) GetVehicle() ProductVehicle {
+func (b *BikeBuilder) GetVehicle() Product {
 	return b.v
 }
 
-// ShuttleBusBuilder is a new vehicle type added is easy.
+// ShuttleBusBuilder is an implementation of the Builder interface
 type ShuttleBusBuilder struct {
-	v ProductVehicle
+	v Product
 }
 
-func (s *ShuttleBusBuilder) SetWheels() BuildProcess {
+func (s *ShuttleBusBuilder) SetWheels() Builder {
 	s.v.Wheels = 4 * 2
 	return s
 }
-func (s *ShuttleBusBuilder) SetSeats() BuildProcess {
+func (s *ShuttleBusBuilder) SetSeats() Builder {
 	s.v.Seats = 30
 	return s
 }
-func (s *ShuttleBusBuilder) SetStructure() BuildProcess {
+func (s *ShuttleBusBuilder) SetStructure() Builder {
 	s.v.Structure = "ShuttleBus"
 	return s
 }
-func (s *ShuttleBusBuilder) GetVehicle() ProductVehicle {
+func (s *ShuttleBusBuilder) GetVehicle() Product {
 	return s.v
 }
