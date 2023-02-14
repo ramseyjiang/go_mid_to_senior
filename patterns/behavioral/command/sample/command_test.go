ee72f609e6fce609cc5095a0f8d77faed79de8a4
ramseyjiang <ramseyjiang@gmail.com>
2023-02-14 20:09:59 +1300

    update comments

--- patterns/behavioral/command/sample/command_test.go
+++ patterns/behavioral/command/sample/command_test.go
@@ -6,6 +6,8 @@ import (
 	"github.com/go-playground/assert/v2"
 )
 
+// The client invokes a command using an Invoker, which calls the command's execute method,
+// which, in turn, delegates to the Receiver's action method.
 func TestExecuteCommand(t *testing.T) {
 	receiver := &ConcreteReceiver{}
 	command := &ConcreteCommand{receiver: receiver}
