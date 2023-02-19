ad6b877d9b1c8019eaf0873ec8d5c561542fcfb6
ramseyjiang <ramseyjiang@gmail.com>
2023-02-19 22:38:33 +1300

    add observer example tests

--- patterns/behavioral/observer/weather/observer_test.go
+++ patterns/behavioral/observer/weather/observer_test.go
@@ -14,17 +14,23 @@ func TestObserver(t *testing.T) {
 	weatherStation.RegisterObserver(phoneDisplay)
 	weatherStation.RegisterObserver(pcDisplay)
 
-	weatherStation.SetTemperature(25.5)
-	assert.Equal(t, 25.5, phoneDisplay.GetTemp())
-	assert.Equal(t, 25.5, pcDisplay.GetTemp())
+	t.Run("Observer notify", func(t *testing.T) {
+		weatherStation.SetTemperature(25.5)
+		assert.Equal(t, 25.5, phoneDisplay.GetTemp())
+		assert.Equal(t, 25.5, pcDisplay.GetTemp())
+	})
 
-	weatherStation.RemoveObserver(phoneDisplay)
-	weatherStation.SetTemperature(30.2)
-	assert.Equal(t, 30.2, pcDisplay.GetTemp())
-	assert.NotEqual(t, 30.2, phoneDisplay.GetTemp()) // because the phoneDisplay observer was removed
+	t.Run("Observer remove one and notify one", func(t *testing.T) {
+		weatherStation.RemoveObserver(phoneDisplay)
+		weatherStation.SetTemperature(30.2)
+		assert.Equal(t, 30.2, pcDisplay.GetTemp())
+		assert.NotEqual(t, 30.2, phoneDisplay.GetTemp()) // because the phoneDisplay observer was removed
+	})
 
-	weatherStation.RemoveObserver(pcDisplay) // it should remove observer first, if not, the observer will also be updated.
-	weatherStation.SetTemperature(24.81)
-	assert.NotEqual(t, 24.81, phoneDisplay.GetTemp()) // because the phoneDisplay observer was removed
-	assert.NotEqual(t, 24.81, pcDisplay.GetTemp())
+	t.Run("Observer remove all and notify none", func(t *testing.T) {
+		weatherStation.RemoveObserver(pcDisplay) // it should remove observer first, if not, the observer will also be updated.
+		weatherStation.SetTemperature(24.81)
+		assert.NotEqual(t, 24.81, phoneDisplay.GetTemp()) // because the phoneDisplay observer was removed
+		assert.NotEqual(t, 24.81, pcDisplay.GetTemp())
+	})
 }
