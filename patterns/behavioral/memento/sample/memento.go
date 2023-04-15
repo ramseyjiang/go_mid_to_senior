35cf51f757584d636e465fc5571fb1fce099f84d
ramseyjiang <ramseyjiang@gmail.com>
2023-04-15 20:55:07 +1200

    update pattern memento

--- patterns/behavioral/memento/sample/memento.go
+++ patterns/behavioral/memento/sample/memento.go
@@ -1,23 +1,10 @@
 package sample
 
-// The Memento struct stores the state of the Originator object.
-type Memento struct {
-	state string
-}
-
 // The Originator struct is the object whose state we want to save and restore
 // If the Memento contains a specific state, the originator type contains the state that is currently loaded.
 // Also, to save the state of something could be as simple as to take some value or as complex as to maintain the state of some distributed application.
 type Originator struct {
 	state string
-}
-
-func (o *Originator) setState(state string) {
-	o.state = state
-}
-
-func (o *Originator) getState() string {
-	return o.state
 }
 
 // createMemento returns a new Memento object containing the current state.
@@ -28,6 +15,19 @@ func (o *Originator) createMemento() *Memento {
 // restoreMemento takes a Memento object and sets the Originator's state to the state stored in the Memento.
 func (o *Originator) restoreMemento(m *Memento) {
 	o.state = m.state
+}
+
+// The Memento struct stores the state of the Originator object.
+type Memento struct {
+	state string
+}
+
+func (o *Originator) setState(state string) {
+	o.state = state
+}
+
+func (o *Originator) getState() string {
+	return o.state
 }
 
 // CareTaker struct, which is responsible for storing and retrieving Memento objects
