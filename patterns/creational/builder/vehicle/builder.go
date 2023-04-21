8f51d0429d74d4caf2e49a0418ae3d3f673c778f
ramseyjiang <ramseyjiang@gmail.com>
2023-04-21 20:39:27 +1200

    update builder pattern

--- patterns/creational/builder/vehicle/builder.go
+++ patterns/creational/builder/vehicle/builder.go
@@ -1,6 +1,15 @@
 package vehicle
 
-// Builder is the interface that specifies the methods for building the parts of the product
+// Step 1: Define the Product with the required properties.
+
+type Product struct {
+	Wheels    int
+	Seats     int
+	Structure string
+}
+
+// Step 2: Create a Builder interface with methods to construct the object
+
 type Builder interface {
 	SetWheels() Builder
 	SetSeats() Builder
@@ -8,29 +17,7 @@ type Builder interface {
 	GetVehicle() Product
 }
 
-// Product represents the complex object that we want to build
-type Product struct {
-	Wheels    int
-	Seats     int
-	Structure string
-}
-
-// ManufactureDirector is responsible for using the Builder interface to build the product
-type ManufactureDirector struct {
-	builder Builder
-}
-
-// SetBuilder allow to change the builder that is being used in the Manufacturing director
-func (f *ManufactureDirector) SetBuilder(b Builder) {
-	f.builder = b
-}
-
-// Construct will use the builder that is stored in Manufacturing, and will reproduce the required steps
-func (f *ManufactureDirector) Construct() {
-	f.builder.SetSeats().SetStructure().SetWheels()
-}
-
-// CarBuilder is an implementation of the Builder interface
+// CarBuilder is used to concrete Builder structs, it is step 3
 type CarBuilder struct {
 	v Product
 }
@@ -48,12 +35,11 @@ func (c *CarBuilder) SetStructure() Builder {
 	return c
 }
 
-// GetVehicle returns the built product
 func (c *CarBuilder) GetVehicle() Product {
 	return c.v
 }
 
-// BikeBuilder is an implementation of the Builder interface
+// BikeBuilder is used to concrete Builder structs
 type BikeBuilder struct {
 	v Product
 }
@@ -62,19 +48,22 @@ func (b *BikeBuilder) SetWheels() Builder {
 	b.v.Wheels = 2
 	return b
 }
+
 func (b *BikeBuilder) SetSeats() Builder {
 	b.v.Seats = 2
 	return b
 }
+
 func (b *BikeBuilder) SetStructure() Builder {
 	b.v.Structure = "Bike"
 	return b
 }
+
 func (b *BikeBuilder) GetVehicle() Product {
 	return b.v
 }
 
-// ShuttleBusBuilder is an implementation of the Builder interface
+// ShuttleBusBuilder is used to concrete Builder structs
 type ShuttleBusBuilder struct {
 	v Product
 }
@@ -83,14 +72,30 @@ func (s *ShuttleBusBuilder) SetWheels() Builder {
 	s.v.Wheels = 4 * 2
 	return s
 }
+
 func (s *ShuttleBusBuilder) SetSeats() Builder {
 	s.v.Seats = 30
 	return s
 }
+
 func (s *ShuttleBusBuilder) SetStructure() Builder {
 	s.v.Structure = "ShuttleBus"
 	return s
 }
+
 func (s *ShuttleBusBuilder) GetVehicle() Product {
 	return s.v
+}
+
+// ManufactureDirector is used to take a Builder interface and construct the object.
+type ManufactureDirector struct {
+	builder Builder
+}
+
+func (f *ManufactureDirector) SetBuilder(b Builder) {
+	f.builder = b
+}
+
+func (f *ManufactureDirector) Construct() {
+	f.builder.SetSeats().SetStructure().SetWheels()
 }
