48d54024cade6cf91c36755d76de047ef120bbc5
ramseyjiang <ramseyjiang@gmail.com>
2023-05-06 22:17:15 +1200

    update visitor pattern example

--- patterns/behavioral/vistor/shop/visitor.go
+++ patterns/behavioral/vistor/shop/visitor.go
@@ -1,84 +1,39 @@
 package shop
 
-// The ProductInfoRetriever type has a method to get the price and the name of the product.
-type ProductInfoRetriever interface {
-	GetPrice() float32
-	GetName() string
+type Item interface {
+	Accept(visitor DiscountVisitor) float64
 }
 
-// The Visitor interface has a Visit method that accepts the ProductInfoRetriever type as a visitor.
-type Visitor interface {
-	Visit(ProductInfoRetriever)
+type DiscountVisitor interface {
+	VisitBook(book *Book) float64
+	VisitElectronic(electronic *Electronic) float64
 }
 
-// Visitable interface has an Accept method that takes a Visitor type as an argument.
-type Visitable interface {
-	Accept(Visitor)
+type Book struct {
+	price float64
 }
 
-// Product type, implemented the ProductInfoRetriever and the Visitable interfaces together, and embedded it on each product.
-// The Product type that can store the information about almost any product of the shop.
-// Notice: one struct implements two interface together.
-type Product struct {
-	Price float32
-	Name  string
+func (b *Book) Accept(visitor DiscountVisitor) float64 {
+	return visitor.VisitBook(b)
 }
 
-func (p *Product) GetPrice() float32 {
-	return p.Price
+type Electronic struct {
+	price float64
 }
 
-func (p *Product) Accept(v Visitor) {
-	v.Visit(p)
+func (e *Electronic) Accept(visitor DiscountVisitor) float64 {
+	return visitor.VisitElectronic(e)
 }
 
-func (p *Product) GetName() string {
-	return p.Name
+type SeasonalDiscountVisitor struct {
+	bookDiscount       float64
+	electronicDiscount float64
 }
 
-// Rice is one type of the product, it has the Product type embedded.
-type Rice struct {
-	Product
+func (v *SeasonalDiscountVisitor) VisitBook(book *Book) float64 {
+	return book.price * (1 - v.bookDiscount)
 }
 
-// Pasta is one type of the product, it has the Product type embedded.
-type Pasta struct {
-	Product
-}
-
-// Fridge is one type of the product, it has the Product type embedded.
-type Fridge struct {
-	Product
-}
-
-// GetPrice overrides GetPrice method of Product type
-func (f *Fridge) GetPrice() float32 {
-	return f.Product.Price + 30
-}
-
-// Accept overrides "Accept" method from Product and implements the Visitable interface
-func (f *Fridge) Accept(v Visitor) {
-	v.Visit(f)
-}
-
-// a couple of Visitors interfaces are below.
-
-// PriceVisitor sums the price of all products
-type PriceVisitor struct {
-	Sum float32
-}
-
-// The Visit takes the value of the Price variable of the ProductInfoRetriever type, passed as an argument, and adds it to the Sum field.
-func (pv *PriceVisitor) Visit(p ProductInfoRetriever) {
-	pv.Sum += p.GetPrice()
-}
-
-// NameVisitor prints the name of each product
-type NameVisitor struct {
-	ProductList []string
-}
-
-// Visit stores the name of the ProductInfoRetriever type, passed as an argument, and appends it to a new line on the ProductList field.
-func (n *NameVisitor) Visit(p ProductInfoRetriever) {
-	n.ProductList = append(n.ProductList, p.GetName())
+func (v *SeasonalDiscountVisitor) VisitElectronic(electronic *Electronic) float64 {
+	return electronic.price * (1 - v.electronicDiscount)
 }
--- patterns/behavioral/vistor/shop/visitor_test.go
+++ patterns/behavioral/vistor/shop/visitor_test.go
@@ -2,48 +2,30 @@ package shop
 
 import (
 	"testing"
-
-	"golang.org/x/exp/slices"
-
-	"github.com/go-playground/assert/v2"
 )
 
-func TestShop(t *testing.T) {
-	products := make([]Visitable, 3)
-	products[0] = &Rice{
-		Product: Product{
-			Price: 22.0,
-			Name:  "Some rice",
-		},
-	}
-	products[1] = &Pasta{
-		Product: Product{
-			Price: 50.0,
-			Name:  "Some pasta",
-		},
-	}
-	products[2] = &Fridge{
-		Product: Product{
-			Price: 70,
-			Name:  "A fridge",
-		},
+func TestDiscounts(t *testing.T) {
+	book := &Book{price: 100}
+	electronic := &Electronic{price: 200}
+
+	seasonalDiscount := &SeasonalDiscountVisitor{
+		bookDiscount:       0.1,
+		electronicDiscount: 0.2,
 	}
 
-	priceVisitor := &PriceVisitor{}
+	t.Run("Test Book price after discount", func(t *testing.T) {
+		discountedBookPrice := book.Accept(seasonalDiscount)
+		expectedBookPrice := 100 * (1 - 0.1)
+		if discountedBookPrice != expectedBookPrice {
+			t.Errorf("Book price after discount is incorrect, expected: %.2f, got: %.2f", expectedBookPrice, discountedBookPrice)
+		}
+	})
 
-	for _, p := range products {
-		p.Accept(priceVisitor)
-	}
-
-	assert.Equal(t, float32(172), priceVisitor.Sum)
-
-	nameVisitor := &NameVisitor{}
-
-	for _, p := range products {
-		p.Accept(nameVisitor)
-	}
-
-	assert.Equal(t, true, slices.Contains(nameVisitor.ProductList, "A fridge"))
-	assert.Equal(t, true, slices.Contains(nameVisitor.ProductList, "Some pasta"))
-	assert.Equal(t, true, slices.Contains(nameVisitor.ProductList, "Some rice"))
+	t.Run("Test Electronic price after discount is incorrect", func(t *testing.T) {
+		discountedElectronicPrice := electronic.Accept(seasonalDiscount)
+		expectedElectronicPrice := 200 * (1 - 0.2)
+		if discountedElectronicPrice != expectedElectronicPrice {
+			t.Errorf("Electronic price after discount is incorrect, expected: %.2f, got: %.2f", expectedElectronicPrice, discountedElectronicPrice)
+		}
+	})
 }
