f249d38a9a490cc5ed5c765aeec06784426e8038
ramseyjiang <ramseyjiang@gmail.com>
2023-05-18 22:52:52 +1200

    add EDD

---
+++ development/edd/event.go
@@ -0,0 +1,35 @@
+package edd
+
+import (
+	"fmt"
+)
+
+// Order data structure
+type Order struct {
+	ID    string
+	Items map[string]int // map of item ID and quantity
+}
+
+type Event interface {
+	ProcessEvent() string
+}
+
+type OrderPlacedEvent struct {
+	Order Order
+}
+
+func (e OrderPlacedEvent) ProcessEvent() string {
+	message := fmt.Sprintf("Order %s has been placed\n", e.Order.ID)
+	// Here you can do things like reserving inventory, notifying the user, etc.
+	return message
+}
+
+type InventoryReservedEvent struct {
+	Order Order
+}
+
+func (e InventoryReservedEvent) ProcessEvent() string {
+	message := fmt.Sprintf("Inventory has been reserved for order %s\n", e.Order.ID)
+	// Here you can do things like preparing the order for shipping, etc.
+	return message
+}
---
+++ development/edd/event_test.go
@@ -0,0 +1,42 @@
+package edd
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestProcessEvent(t *testing.T) {
+	order := Order{
+		ID: "1234",
+		Items: map[string]int{
+			"item1": 1,
+			"item2": 2,
+		},
+	}
+
+	tests := []struct {
+		name     string
+		event    Event
+		expected string
+	}{
+		{
+			name:     "OrderPlacedEvent",
+			event:    OrderPlacedEvent{Order: order},
+			expected: fmt.Sprintf("Order %s has been placed\n", order.ID),
+		},
+		{
+			name:     "InventoryReservedEvent",
+			event:    InventoryReservedEvent{Order: order},
+			expected: fmt.Sprintf("Inventory has been reserved for order %s\n", order.ID),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := test.event.ProcessEvent()
+			if result != test.expected {
+				t.Errorf("expected %s, got %s", test.expected, result)
+			}
+		})
+	}
+}
