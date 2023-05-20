e4403e4aec36e68a77b5c836347f0833b69210f1
ramseyjiang <ramseyjiang@gmail.com>
2023-05-20 17:11:59 +1200

    update

--- development/bdd/booking-godog/booking_test.go
+++ development/bdd/booking-godog/booking_test.go
@@ -1,66 +1,10 @@
-package booking_test
+package booking
 
 import (
 	"testing"
 
 	"github.com/cucumber/godog"
 )
-
-type Booking struct {
-	// Fields related to booking
-	// (city, date range, hotel room, user info, payment info, etc.)
-	// You'd typically have complex types or interfaces here
-	// but for simplicity let's use basic types.
-	city          string
-	dateRange     string
-	hotelRoom     string
-	personalInfo  string
-	paymentInfo   string
-	isConfirmed   bool
-	receivedEmail bool
-}
-
-func (b *Booking) SelectedAValidCityAndDateRange() error {
-	// Implement logic for selecting a valid city and date range
-	// Here we just hardcode some values
-	b.city = "San Francisco"
-	b.dateRange = "2023-07-10 to 2023-07-15"
-
-	return nil
-}
-
-func (b *Booking) SelectASpecificHotelRoom() error {
-	// Implement logic for selecting a specific hotel room
-	// Here we just hardcode some value
-	b.hotelRoom = "Deluxe Suite"
-
-	return nil
-}
-
-func (b *Booking) InputValidPersonalAndPaymentInformation() error {
-	// Implement logic for inputting personal and payment information
-	// Here we just hardcode some values
-	b.personalInfo = "John Doe"
-	b.paymentInfo = "Valid Credit Card"
-
-	return nil
-}
-
-func (b *Booking) BookingIsConfirmed() error {
-	// Implement logic for confirming booking
-	// For simplicity, let's just set the field to true
-	b.isConfirmed = true
-
-	return nil
-}
-
-func (b *Booking) ReceiveABookingConfirmationEmail() error {
-	// Implement logic for receiving a booking confirmation email
-	// For simplicity, let's just set the field to true
-	b.receivedEmail = true
-
-	return nil
-}
 
 func InitializeScenario(ctx *godog.ScenarioContext) {
 	booking := &Booking{}
