328a27789113f265b057fa7499ee4dbe531f578f
ramseyjiang <ramseyjiang@gmail.com>
2023-06-02 23:05:45 +1200

    update observer pattern

--- patterns/behavioral/observer/publisher/observer.go
+++ patterns/behavioral/observer/publisher/observer.go
@@ -2,58 +2,65 @@ package publisher
 
 import "fmt"
 
-// Event defines an indication of a some occurrence.
-type Event struct {
-	Data string
+// Step 1: Define a subject interface and an observer interface.
+
+// Subject represents the subject being observed.
+type Subject interface {
+	RegisterObserver(observer Observer)
+	RemoveObserver(observer Observer)
+	NotifyObservers(message string)
 }
 
-// Observer defines a standard interface to listen for a specific event.
+// Observer represents the observer.
 type Observer interface {
-	// Notify allows to publish an event.
-	Notify(Event)
+	Update(message string)
 }
 
-// Notifier is the instance being observed.
-type Notifier interface {
-	AddObserver(Observer)
-	RemoveObserver(Observer)
-	NotifyObservers(Event)
+// Step 2: Implement the subject interface.
+
+// ConcreteSubject represents the concrete implementation of the Subject interface.
+type ConcreteSubject struct {
+	observers []Observer
 }
 
-// Publisher struct, which is the one that triggers an event, so it must accept new observers and remove them if necessary.
-// When the Publisher struct is triggered, it must notify all its observers of the new event with the data associated.
-// Publisher structure stores the list of subscribed observers in a slice field called ObserversList.
-type Publisher struct {
-	ObserversList []Observer
+// RegisterObserver adds an observer to the list of observers.
+func (s *ConcreteSubject) RegisterObserver(observer Observer) {
+	s.observers = append(s.observers, observer)
 }
 
-func (s *Publisher) AddObserver(o Observer) {
-	s.ObserversList = append(s.ObserversList, o)
-}
+// RemoveObserver removes an observer from the list of observers.
+func (s *ConcreteSubject) RemoveObserver(observer Observer) {
+	indexToRemove := -1
 
-func (s *Publisher) RemoveObserver(o Observer) {
-	var indexToRemove int
-
-	for i, observer := range s.ObserversList {
-		// comparing the Observer object's o variable with the ones stored in the list
-		if observer == o {
+	// Find the index of the observer to remove
+	for i, obs := range s.observers {
+		if obs == observer {
 			indexToRemove = i
 			break
 		}
 	}
 
-	// 1. First, we need to use slice indexing to return a new slice containing every object from the beginning of the slice to the index we want to remove (not included).
-	// 2. Then, we get another slice from the index we want to remove (not included) to the last object in the slice
-	// 3. Finally, we join the previous two new slices into a new one (the append function)
-	// For example, in a list from 1 to 10 in which we want to remove the number 5,
-	// we have to create a new slice, joining a slice from 1 to 4 and a slice from 6 to 10.
-	s.ObserversList = append(s.ObserversList[:indexToRemove], s.ObserversList[indexToRemove+1:]...)
+	// Remove the observer from the list
+	if indexToRemove >= 0 {
+		s.observers = append(s.observers[:indexToRemove], s.observers[indexToRemove+1:]...)
+	}
 }
 
-// NotifyObservers method with a string that acts as the message we want to spread between all observers.
-func (s *Publisher) NotifyObservers(e Event) {
-	fmt.Printf("Publisher received message '%s' to notify observers\n", e)
-	for _, observer := range s.ObserversList {
-		observer.Notify(e)
+// NotifyObservers notifies all observers with the given message.
+func (s *ConcreteSubject) NotifyObservers(message string) {
+	for _, observer := range s.observers {
+		observer.Update(message)
 	}
+}
+
+// Step 3: Implement the observer interface.
+
+// ConcreteObserver represents the concrete implementation of the Observer interface.
+type ConcreteObserver struct {
+	name string
+}
+
+// Update prints the message received by the observer.
+func (o *ConcreteObserver) Update(message string) {
+	fmt.Printf("%s received: %s\n", o.name, message)
 }
--- patterns/behavioral/observer/publisher/observer_test.go
+++ patterns/behavioral/observer/publisher/observer_test.go
@@ -2,89 +2,78 @@ package publisher
 
 import (
 	"testing"
-
-	"github.com/go-playground/assert/v2"
 )
 
 type TestObserver struct {
-	ID      int
-	Message Event
+	Name     string
+	Messages []string
 }
 
-// TestObserver structure implements the Observer pattern by defining Notify(string) method
-func (p *TestObserver) Notify(e Event) {
-	p.Message = e
+func (o *TestObserver) Update(message string) {
+	o.Messages = append(o.Messages, message)
 }
 
-// given a different ID to each observer so that we can see later that each of them has printed the expected message.
-func TestSubject(t *testing.T) {
-	testObserver1 := &TestObserver{1, Event{"default"}}
-	testObserver2 := &TestObserver{2, Event{"default"}}
-	testObserver3 := &TestObserver{3, Event{"default"}}
+func TestPublisher(t *testing.T) {
+	tests := []struct {
+		name         string
+		subscribers  []Observer
+		message      string
+		expectedLogs map[string][]string
+	}{
+		{
+			name: "Single Subscriber",
+			subscribers: []Observer{
+				&TestObserver{Name: "Observer 1"},
+			},
+			message: "Hello, World!",
+			expectedLogs: map[string][]string{
+				"Observer 1": {"Hello, World!"},
+			},
+		},
+		{
+			name: "Multiple Subscribers",
+			subscribers: []Observer{
+				&TestObserver{Name: "Observer 1"},
+				&TestObserver{Name: "Observer 2"},
+			},
+			message: "Hello, World!",
+			expectedLogs: map[string][]string{
+				"Observer 1": {"Hello, World!"},
+				"Observer 2": {"Hello, World!"},
+			},
+		},
+		{
+			name:         "No Subscribers",
+			subscribers:  []Observer{},
+			message:      "Hello, World!",
+			expectedLogs: map[string][]string{},
+		},
+	}
 
-	publisher := Publisher{}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			publisher := &ConcreteSubject{}
 
-	// add the observers by calling the AddObserver method on the Publisher structure.
-	t.Run("AddObserver", func(t *testing.T) {
-		publisher.AddObserver(testObserver1)
-		publisher.AddObserver(testObserver2)
-		publisher.AddObserver(testObserver3)
-
-		assert.Equal(t, len(publisher.ObserversList), 3)
-		if len(publisher.ObserversList) != 3 {
-			t.Fail()
-		}
-	})
-
-	t.Run("RemoveObserver", func(t *testing.T) {
-		// RemoveObserver takes the observer with ID 2 and remove it from the list.
-		publisher.RemoveObserver(testObserver2)
-
-		assert.Equal(t, len(publisher.ObserversList), 2)
-
-		for _, observer := range publisher.ObserversList {
-			// make a type casting from a pointer to an observer, to a pointer to the TestObserver structure,
-			// and check that the casting has been done correctly.
-			testObserver, ok := observer.(*TestObserver)
-			if !ok {
-				t.Fail() // This marks the test as failed, but allows the execution to continue.
+			for _, observer := range test.subscribers {
+				publisher.RegisterObserver(observer)
 			}
 
-			assert.NotEqual(t, testObserver.ID, 2)
-			// check that none of the observers left have the ID 2 because it must be removed.
-			if testObserver.ID == 2 {
-				t.Fail()
+			publisher.NotifyObservers(test.message)
+
+			for _, observer := range test.subscribers {
+				expectedLogs := test.expectedLogs[observer.(*TestObserver).Name]
+				receivedLogs := observer.(*TestObserver).Messages
+
+				if len(receivedLogs) != len(expectedLogs) {
+					t.Errorf("expected %d log(s) for observer %s, got %d", len(expectedLogs), observer.(*TestObserver).Name, len(receivedLogs))
+				}
+
+				for i := range receivedLogs {
+					if receivedLogs[i] != expectedLogs[i] {
+						t.Errorf("observer %s: expected log %q, got %q", observer.(*TestObserver).Name, expectedLogs[i], receivedLogs[i])
+					}
+				}
 			}
-		}
-	})
-
-	// When using the Notify method, all instances of TestObserver structure must change their Message field from empty to the passed message
-	t.Run("Notify", func(t *testing.T) {
-		assert.NotEqual(t, len(publisher.ObserversList), 0)
-
-		for _, observer := range publisher.ObserversList {
-			// make a type casting from a pointer to an observer, to a pointer to the TestObserver structure,
-			// and check that the casting has been done correctly.
-			printObserver, ok := observer.(*TestObserver)
-			if !ok {
-				t.Fail()
-				break
-			}
-
-			assert.Equal(t, printObserver.Message, Event{"default"})
-		}
-
-		message := Event{"Hello World!"}
-		publisher.NotifyObservers(Event{"Hello World!"})
-
-		for _, observer := range publisher.ObserversList {
-			printObserver, ok := observer.(*TestObserver)
-			if !ok {
-				t.Fail()
-				break
-			}
-
-			assert.Equal(t, printObserver.Message, message)
-		}
-	})
+		})
+	}
 }
