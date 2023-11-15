2b843e51830150ca150c0c545d5d7e2685a7d93e
ramseyjiang <ramseyjiang@gmail.com>
2023-11-16 11:34:14 +1300

    add reverse string

---
+++ interviews/str/impl.go
@@ -0,0 +1,18 @@
+package str
+
+func ReverseStr(s []byte) {
+	i := 0
+	j := len(s) - 1
+
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+		j--
+		i++
+	}
+}
+
+func ReverseStr2(s []byte) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
---
+++ interviews/str/impl_test.go
@@ -0,0 +1,34 @@
+package str
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReverseStr(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []byte
+		want  []byte
+	}{
+		{
+			name:  "test 1",
+			input: []byte{'h', 'e', 'l', 'l', 'o'},
+			want:  []byte{'o', 'l', 'l', 'e', 'h'},
+		},
+		{
+			"test 2",
+			[]byte{'H', 'a', 'n', 'n', 'a', 'h'},
+			[]byte{'h', 'a', 'n', 'n', 'a', 'H'},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ReverseStr(tt.input)
+			if !reflect.DeepEqual(tt.input, tt.want) {
+				t.Errorf("rotate() = %v, want %v", tt.input, tt.want)
+			}
+		})
+	}
+}
