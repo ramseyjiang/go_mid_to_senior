a15e3877f93ea34eb2e5b094cfe61235ce15a52a
ramseyjiang <ramseyjiang@gmail.com>
2023-11-20 10:10:32 +1300

    update rotate

--- interviews/array/rotate/impl.go
+++ interviews/array/rotate/impl.go
@@ -14,11 +14,13 @@ func Rotate(nums []int, k int) {
 }
 
 func Rotate2(nums []int, k int) {
-	tmp := make([]int, len(nums)-1)
+	n := len(nums)
+	k %= n // Ensure k is within the bounds of nums length
+	tmp := make([]int, n-1)
 	for i := 1; i <= k; i++ {
-		tmpLast := nums[len(nums)-1]
-		front := nums[0 : len(nums)-1]
+		tmpLast := nums[n-1]
+		front := nums[0 : n-1]
 		tmp = append([]int{tmpLast}, front...)
-		copy(nums, tmp)
+		copy(nums, tmp) // The copy is in the loop, it will waste space.
 	}
 }
