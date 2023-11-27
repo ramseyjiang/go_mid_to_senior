f6cb50b8f746def81ad1a24b87bf8e1f37cacd6b
ramseyjiang <ramseyjiang@gmail.com>
2023-11-28 10:37:38 +1300

    update reverse str

--- interviews/str/reversestr/impl.go
+++ interviews/str/reversestr/impl.go
@@ -1,6 +1,12 @@
 package reversestr
 
 func ReverseStr(s []byte) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func ReverseStr2(s []byte) {
 	i := 0
 	j := len(s) - 1
 
@@ -8,11 +14,5 @@ func ReverseStr(s []byte) {
 		s[i], s[j] = s[j], s[i]
 		j--
 		i++
-	}
-}
-
-func ReverseStr2(s []byte) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
 	}
 }
