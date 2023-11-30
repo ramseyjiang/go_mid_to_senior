cda462757cbb89f0df90544ce89d86ae06daaa3b
ramseyjiang <ramseyjiang@gmail.com>
2023-11-30 15:48:58 +1300

    update first unique

--- interviews/str/firstunique/impl.go
+++ interviews/str/firstunique/impl.go
@@ -3,10 +3,12 @@ package firstunique
 func firstUniqueChar(s string) int {
 	charCount := [26]int{}
 
+	// First step: count the frequency of each character
 	for i := range s {
 		charCount[s[i]-'a']++
 	}
 
+	// Second step: find the first unique character
 	for k := range s {
 		if charCount[s[k]-'a'] == 1 {
 			return k
@@ -19,12 +21,12 @@ func firstUniqueChar(s string) int {
 func firstUniqueChar2(s string) int {
 	charCount := make(map[rune]int)
 
-	// First pass: count the frequency of each character
+	// First step: count the frequency of each character
 	for _, char := range s {
 		charCount[char]++
 	}
 
-	// Second pass: find the first unique character
+	// Second step: find the first unique character
 	for i, char := range s {
 		if charCount[char] == 1 {
 			return i
