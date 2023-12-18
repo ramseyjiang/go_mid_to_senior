6f8b7d6279bf2f32fc0354c16b594257b4d55f9e
ramseyjiang <ramseyjiang@gmail.com>
2023-12-19 10:32:55 +1300

    Add stocktrade example

---
+++ patterns/concurrency/ratelimit/slidewindow/stocktrade/impl.go
@@ -0,0 +1,61 @@
+package stocktrade
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type TradeWindow struct {
+	Trades []time.Time
+	Mutex  sync.Mutex
+}
+
+func NewTradeWindow() *TradeWindow {
+	return &TradeWindow{
+		Trades: []time.Time{},
+	}
+}
+
+func (tw *TradeWindow) AddTrade(t time.Time) {
+	tw.Mutex.Lock()
+	defer tw.Mutex.Unlock()
+	tw.Trades = append(tw.Trades, t)
+}
+
+func (tw *TradeWindow) AllowTrade() bool {
+	tw.Mutex.Lock()
+	defer tw.Mutex.Unlock()
+
+	now := time.Now()
+	oneMinuteAgo := now.Add(-1 * time.Minute)
+
+	// Remove trades older than 1 minute
+	validTrades := []time.Time{}
+	for _, tradeTime := range tw.Trades {
+		if tradeTime.After(oneMinuteAgo) {
+			validTrades = append(validTrades, tradeTime)
+		}
+	}
+	tw.Trades = validTrades
+
+	// Allow trade if less than 5 trades in the last minute
+	return len(tw.Trades) < 5
+}
+
+func TradeLimitMiddleware(tw *TradeWindow, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !tw.AllowTrade() {
+			http.Error(w, "Trade limit exceeded", http.StatusTooManyRequests)
+			return
+		}
+
+		tw.AddTrade(time.Now())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func TradeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Trade executed successfully")
+}
---
+++ patterns/concurrency/ratelimit/slidewindow/stocktrade/impl_test.go
@@ -0,0 +1,48 @@
+package stocktrade
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestTradeLimitMiddleware(t *testing.T) {
+	tw := NewTradeWindow()
+
+	tests := []struct {
+		name           string
+		numberOfTrades int
+		sleepDuration  time.Duration
+		expectedStatus int
+	}{
+		{"UnderLimit", 4, 0, http.StatusOK},
+		{"AtLimit", 5, 0, http.StatusOK},
+		{"OverLimit", 6, 0, http.StatusTooManyRequests},
+		{"ResetAfterOneMinute", 6, 1 * time.Minute, http.StatusOK},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler := TradeLimitMiddleware(tw, http.HandlerFunc(TradeHandler))
+
+			for i := 0; i < tt.numberOfTrades; i++ {
+				if i == 5 && tt.sleepDuration > 0 {
+					time.Sleep(tt.sleepDuration)
+				}
+
+				req := httptest.NewRequest("POST", "/trade", nil)
+				rr := httptest.NewRecorder()
+
+				handler.ServeHTTP(rr, req)
+
+				if i == tt.numberOfTrades-1 {
+					if status := rr.Code; status != tt.expectedStatus {
+						t.Errorf("handler returned wrong status code: got %v want %v",
+							status, tt.expectedStatus)
+					}
+				}
+			}
+		})
+	}
+}
