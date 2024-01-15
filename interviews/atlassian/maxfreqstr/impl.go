3d90a80f091ffe8e90c980ec25fd759149c595bf
ramseyjiang <ramseyjiang@gmail.com>
2024-01-15 22:26:16 +1300

    add max freq substr

---
+++ interviews/atlassian/maxfreqstr/impl.go
@@ -0,0 +1,51 @@
+package maxfreqstr
+
+func maxFreqStr(s string, maxLetters int, minSize int, maxSize int) int {
+
+	subs := map[string]int{}
+	maxFreq := 0
+
+	for i := 0; i <= len(s)-minSize; i++ {
+
+		substr := s[i : i+minSize]
+		ch := map[rune]int{}
+		for _, x := range substr {
+			ch[x] += 1
+		}
+		if len(ch) <= maxLetters {
+			subs[substr] += 1
+			maxFreq = max(maxFreq, subs[substr])
+		}
+	}
+
+	return maxFreq
+
+}
+
+func maxFreqStr2(s string, maxLetters int, minSize int, maxSize int) int {
+	count := make(map[string]int)
+	maxFreq := 0
+
+	for i := 0; i <= len(s)-minSize; i++ {
+		substr := s[i : i+minSize]
+		if isValid(substr, maxLetters) {
+			count[substr]++
+			if count[substr] > maxFreq {
+				maxFreq = count[substr]
+			}
+		}
+	}
+
+	return maxFreq
+}
+
+func isValid(substr string, maxLetters int) bool {
+	uniqueLetters := make(map[rune]int)
+	for _, char := range substr {
+		uniqueLetters[char]++
+		if len(uniqueLetters) > maxLetters {
+			return false
+		}
+	}
+	return true
+}
---
+++ interviews/atlassian/maxfreqstr/impl_test.go
@@ -0,0 +1,41 @@
+package maxfreqstr
+
+import "testing"
+
+func TestMaxFreqStr(t *testing.T) {
+	tests := []struct {
+		name       string
+		inputStr   string
+		maxLetters int
+		minSize    int
+		maxSize    int
+		expected   int
+	}{
+		{
+			name:       "Example 1",
+			inputStr:   "aababcaab",
+			maxLetters: 2,
+			minSize:    3,
+			maxSize:    4,
+			expected:   2,
+		},
+		{
+			name:       "Example 2",
+			inputStr:   "aaaa",
+			maxLetters: 1,
+			minSize:    3,
+			maxSize:    3,
+			expected:   2,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			output := maxFreqStr(tt.inputStr, tt.maxLetters, tt.minSize, tt.maxSize)
+
+			if tt.expected != output {
+				t.Errorf("maxFreStr() output is %v, expected output is %v", output, tt.expected)
+			}
+		})
+	}
+}
