6d35e59d97cb803b83e2c11e78ea86728f44765b
ramseyjiang <ramseyjiang@gmail.com>
2024-01-16 12:13:43 +1300

    update

--- interviews/atlassian/snake/impl.go
+++ interviews/atlassian/snake/impl.go
@@ -41,24 +41,27 @@ func (sg *SnakeGame) Move(direction string) int {
 		return -1
 	}
 
-	// Please notice: the food coord, y is the first, x is the second
+	// Please notice: the food coord, y is the first, x is the second.
+	// Use the snake head position to check whether the snake ate or not.
 	ateFood := sg.foodIndex < len(sg.food) && head.y == sg.food[sg.foodIndex][0] && head.x == sg.food[sg.foodIndex][1]
 	if ateFood {
 		sg.score++
 		sg.foodIndex++
+	} else {
+		// update snake coord, at the beginning, the snake is no body.
+		// If no food is eaten, the last segment of the snake (sg.snake[:len(sg.snake)-1]) is removed to simulate the snake's movement.
+		sg.snake = sg.snake[:len(sg.snake)-1]
 	}
 
+	// Self-Collision Check
 	for i := 0; i < len(sg.snake)-1; i++ {
 		if head.y == sg.snake[i].y && head.x == sg.snake[i].x {
 			return -1
 		}
 	}
 
+	// update the snake length and position each move is end.
 	sg.snake = append([]Coord{head}, sg.snake...)
-
-	if !ateFood {
-		sg.snake = sg.snake[:len(sg.snake)-1]
-	}
 
 	return sg.score
 }
