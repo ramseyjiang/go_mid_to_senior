9f7267bf4404d024573fe9a2c84f5892eeb1d3da
ramseyjiang <ramseyjiang@gmail.com>
2024-01-17 11:51:05 +1300

    add online election

---
+++ interviews/atlassian/onlineelection/impl.go
@@ -0,0 +1,31 @@
+package onlineelection
+
+import "sort"
+
+type TopVotedCandidate struct {
+	persons []int
+	times   []int
+}
+
+func Constructor(persons []int, times []int) TopVotedCandidate {
+	counts := make([]int16, len(persons))
+	var maxCount int16
+	var maxPerson int
+	for i, person := range persons {
+		counts[person]++
+		if counts[person] >= maxCount {
+			maxCount = counts[person]
+			maxPerson = person
+		}
+		persons[i] = maxPerson
+	}
+	return TopVotedCandidate{
+		persons: persons,
+		times:   times,
+	}
+}
+
+func (tvc *TopVotedCandidate) Query(t int) int {
+	i := sort.Search(len(tvc.times), func(i int) bool { return tvc.times[i] > t })
+	return tvc.persons[i-1]
+}
---
+++ interviews/atlassian/onlineelection/impl_test.go
@@ -0,0 +1,34 @@
+package onlineelection
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTopVotedCandidate(t *testing.T) {
+	persons := []int{0, 1, 1, 0, 0, 1, 0}
+	times := []int{0, 5, 10, 15, 20, 25, 30}
+	tvc := Constructor(persons, times)
+
+	tests := []struct {
+		t        int
+		expected int
+	}{
+		{3, 0},
+		{12, 1},
+		{25, 1},
+		{15, 0},
+		{24, 0},
+		{8, 1},
+		// Additional test cases here
+	}
+
+	for _, tt := range tests {
+		t.Run("Query at time "+fmt.Sprint(tt.t), func(t *testing.T) {
+			got := tvc.Query(tt.t)
+			if got != tt.expected {
+				t.Errorf("Q(%d) = %d, want %d", tt.t, got, tt.expected)
+			}
+		})
+	}
+}
