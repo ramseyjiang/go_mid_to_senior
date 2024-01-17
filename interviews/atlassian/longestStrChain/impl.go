4078061985b4a81693a328fd83e3f25cbafc6c9a
ramseyjiang <ramseyjiang@gmail.com>
2024-01-18 10:34:13 +1300

    update

--- interviews/atlassian/longestStrChain/impl.go
+++ interviews/atlassian/longestStrChain/impl.go
@@ -17,11 +17,11 @@ func longestStrChan(words []string) int {
 		longestChain[word] = 1
 		for i := 0; i < len(word); i++ {
 			// Concatenates two substrings, without the character at position i.
-			// It is used to check whether the prev string already in the longest chain.
-			prev := word[:i] + word[i+1:]
+			// It is used to check whether the substr string already in the longest chain.
+			substr := word[:i] + word[i+1:]
 
 			// if the prev has already in, the longest chain length adds 1.
-			if chainLen, exists := longestChain[prev]; exists && chainLen+1 > longestChain[word] {
+			if chainLen, exists := longestChain[substr]; exists && chainLen+1 > longestChain[word] {
 				longestChain[word] = chainLen + 1
 			}
 		}
