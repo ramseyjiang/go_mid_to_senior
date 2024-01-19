3b049573b97580dc3c8c2e05551ff34dc1736710
ramseyjiang <ramseyjiang@gmail.com>
2024-01-19 15:45:37 +1300

    update robot parts

--- interviews/atlassian/robotparts/impl.go
+++ interviews/atlassian/robotparts/impl.go
@@ -7,24 +7,24 @@ func GetRobotParts(allParts []string, requiredParts string) []string {
 	parts_needed := strings.Split(requiredParts, ",")
 
 	// Map to hold the parts each robot has
-	robot_parts := make(map[string]map[string]bool)
+	robotParts := make(map[string]map[string]bool)
 
 	// Iterate over all parts and populate the robot_parts map
 	for _, part := range allParts {
 		split := strings.Split(part, "_")
 		robotName, partName := split[0], split[1]
 
-		if robot_parts[robotName] == nil {
-			robot_parts[robotName] = make(map[string]bool)
+		if robotParts[robotName] == nil {
+			robotParts[robotName] = make(map[string]bool)
 		}
-		robot_parts[robotName][partName] = true
+		robotParts[robotName][partName] = true
 	}
 
 	// List to hold the names of robots that can be completely built
-	complete_robots := []string{}
+	completeRobots := []string{}
 
 	// Check each robot for required parts
-	for robot, parts := range robot_parts {
+	for robot, parts := range robotParts {
 		complete := true
 		for _, needed := range parts_needed {
 			if !parts[needed] {
@@ -33,9 +33,9 @@ func GetRobotParts(allParts []string, requiredParts string) []string {
 			}
 		}
 		if complete {
-			complete_robots = append(complete_robots, robot)
+			completeRobots = append(completeRobots, robot)
 		}
 	}
 
-	return complete_robots
+	return completeRobots
 }
