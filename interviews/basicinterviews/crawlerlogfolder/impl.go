552edfd58619dec881d952c5f2389b3e9e7a5d03
ramseyjiang <ramseyjiang@gmail.com>
2024-05-15 11:12:47 +1200

    update crawler log folder

--- interviews/basicinterviews/crawlerlogfolder/impl.go
+++ interviews/basicinterviews/crawlerlogfolder/impl.go
@@ -20,3 +20,21 @@ func MinOperations(logs []string) int {
 
 	return result
 }
+
+func MinOperations2(logs []string) int {
+	stack := make([]string, 0)
+
+	for i := range logs {
+		switch logs[i] {
+		case "../":
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+		case "./":
+		default:
+			stack = append(stack, logs[i])
+		}
+	}
+
+	return len(stack)
+}
