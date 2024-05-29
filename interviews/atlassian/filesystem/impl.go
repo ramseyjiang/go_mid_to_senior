de62c76da9086612e765c202fe77dc5ab4930450
ramseyjiang <ramseyjiang@gmail.com>
2024-05-29 20:13:22 +1200

    update file design system

--- interviews/atlassian/filesystem/impl.go
+++ interviews/atlassian/filesystem/impl.go
@@ -39,3 +39,86 @@ func (fs *FileSystem) Get(path string) int {
 	}
 	return -1 // Path doesn't exist
 }
+
+/*
+package filesystem
+
+import (
+	"strings"
+)
+
+type TrieNode struct {
+	children map[string]*TrieNode
+	value    int
+	isEnd    bool
+}
+
+type FileSystem struct {
+	root *TrieNode
+}
+
+func NewFileSystem() FileSystem {
+	return FileSystem{
+		root: &TrieNode{
+			children: make(map[string]*TrieNode),
+		},
+	}
+}
+
+func (fs *FileSystem) CreatePath(path string, value int) bool {
+	if path == "" || path == "/" || fs.root == nil {
+		return false
+	}
+
+	parts := strings.Split(path, "/")[1:] // split and remove the first empty part
+	node := fs.root
+
+	for i, part := range parts {
+		if next, exists := node.children[part]; exists {
+			node = next
+		} else {
+			if i == len(parts)-1 {
+				// Only create the final part if we're at the end of the path
+				node.children[part] = &TrieNode{
+					children: make(map[string]*TrieNode),
+					value:    value,
+					isEnd:    true,
+				}
+				return true
+			}
+			return false
+		}
+	}
+
+	// If we exit the loop without creating a new node, the path already exists
+	if node.isEnd {
+		return false
+	}
+
+	node.value = value
+	node.isEnd = true
+	return true
+}
+
+func (fs *FileSystem) Get(path string) int {
+	if path == "" || path == "/" || fs.root == nil {
+		return -1
+	}
+
+	parts := strings.Split(path, "/")[1:] // split and remove the first empty part
+	node := fs.root
+
+	for _, part := range parts {
+		if next, exists := node.children[part]; exists {
+			node = next
+		} else {
+			return -1
+		}
+	}
+
+	if node.isEnd {
+		return node.value
+	}
+	return -1
+}
+*/
