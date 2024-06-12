832359c5f0632cb71de483f9cde2e0080f4b5be9
ramseyjiang <ramseyjiang@gmail.com>
2024-06-12 21:34:57 +1000

    update reverse list

--- interviews/linkedlist/reverselist/impl.go
+++ interviews/linkedlist/reverselist/impl.go
@@ -5,15 +5,28 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func reverseList(head *ListNode) *ListNode {
-	var rev *ListNode
+// Iteratively reverse the linked list
+func reverseListIterative(head *ListNode) *ListNode {
+	var prev *ListNode
 	curr := head
 
 	for curr != nil {
 		nextTemp := curr.Next
-		curr.Next = rev
-		rev = curr
+		curr.Next = prev
+		prev = curr
 		curr = nextTemp
 	}
-	return rev
+	return prev
+}
+
+// Recursively reverse the linked list
+func reverseListRecursive(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	newHead := reverseListRecursive(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
 }
--- interviews/linkedlist/reverselist/impl_test.go
+++ interviews/linkedlist/reverselist/impl_test.go
@@ -5,33 +5,21 @@ import (
 	"testing"
 )
 
-func TestReverseList(t *testing.T) {
-	tests := []struct {
-		name     string
-		head     []int
-		expected []int
-	}{
-		{
-			name:     "Test 1",
-			head:     []int{1, 2, 3, 4, 5},
-			expected: []int{5, 4, 3, 2, 1},
-		},
-		{
-			name:     "Test 2",
-			head:     []int{1, 2},
-			expected: []int{2, 1},
-		},
-		{
-			name:     "Test 3",
-			head:     []int{},
-			expected: []int{},
-		},
-	}
+var tests = []struct {
+	name     string
+	head     []int
+	expected []int
+}{
+	{name: "Test 1", head: []int{1, 2, 3, 4, 5}, expected: []int{5, 4, 3, 2, 1}},
+	{name: "Test 2", head: []int{1, 2}, expected: []int{2, 1}},
+	{name: "Test 3", head: []int{}, expected: []int{}},
+}
 
+func TestReverseListIterative(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			head := sliceToList(tt.head)
-			result := reverseList(head)
+			result := reverseListIterative(head)
 			resultArr := listToSlice(result)
 
 			if len(resultArr) == 0 && len(tt.expected) == 0 {
@@ -40,7 +28,26 @@ func TestReverseList(t *testing.T) {
 			}
 
 			if !reflect.DeepEqual(resultArr, tt.expected) {
-				t.Errorf("reverseList(%v) = %v; expected %v", tt.head, resultArr, tt.expected)
+				t.Errorf("reverseListIterative(%v) = %v; expected %v", tt.head, resultArr, tt.expected)
+			}
+		})
+	}
+}
+
+func TestReverseListRecursive(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			head := sliceToList(tt.head)
+			result := reverseListRecursive(head)
+			resultArr := listToSlice(result)
+
+			if len(resultArr) == 0 && len(tt.expected) == 0 {
+				// Both are empty, test passes
+				return
+			}
+
+			if !reflect.DeepEqual(resultArr, tt.expected) {
+				t.Errorf("reverseListRecursive(%v) = %v; expected %v", tt.head, resultArr, tt.expected)
 			}
 		})
 	}
