7e4d348259808356cf6c1d3acbc738708286100a
ramseyjiang <ramseyjiang@gmail.com>
2024-10-19 15:57:50 +1000

    update

--- interviews/atlassian/electionwinner/impl.go
+++ interviews/atlassian/electionwinner/impl.go
@@ -4,9 +4,27 @@ import (
 	"slices"
 )
 
+func winner(votes []string) (winner string, max int) {
+	counts := make(map[string]int)
+
+	for _, v := range votes {
+		counts[v]++
+		if counts[v] > max {
+			max = counts[v]
+			winner = v
+		}
+
+		if counts[v] == max && len(v) < len(winner) {
+			winner = v
+		}
+	}
+
+	return
+}
+
 // winner finds the election winner from the slice of votes
 // it returns the winning candidate's name and the number of votes received.
-func winner(votes []string) (winnerStr string, maxVotes int) {
+func winner2(votes []string) (winnerStr string, maxVotes int) {
 	voteCounts := make(map[string]int)
 	var candidatesMaxVotes []string
 
@@ -16,7 +34,7 @@ func winner(votes []string) (winnerStr string, maxVotes int) {
 		if voteCounts[vote] > maxVotes {
 			maxVotes = voteCounts[vote]
 			// Reset the slice as we have a new candidate with more votes
-			candidatesMaxVotes = []string{vote}
+			candidatesMaxVotes = []string{vote} // Here, cannot use append to replace, because it is only one winner.
 		} else if voteCounts[vote] == maxVotes {
 			// Append candidate to the slice for further tie-breaking
 			candidatesMaxVotes = append(candidatesMaxVotes, vote)
@@ -28,25 +46,4 @@ func winner(votes []string) (winnerStr string, maxVotes int) {
 	winnerStr = candidatesMaxVotes[0] // The first candidate after sorting is the lexicographically smallest
 
 	return winnerStr, maxVotes
-}
-
-func winner2(votes []string) (winnerStr string, maxVotes int) {
-	voteCounts := make(map[string]int)
-
-	for _, v := range votes {
-		voteCounts[v]++
-	}
-
-	for k, value := range voteCounts {
-		if value > maxVotes {
-			maxVotes = value
-			winnerStr = k
-		}
-
-		if value == maxVotes && len(k) < len(winnerStr) {
-			winnerStr = k
-		}
-	}
-
-	return
 }
