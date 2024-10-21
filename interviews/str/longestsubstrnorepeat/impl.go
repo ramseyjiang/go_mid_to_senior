b7b7abb7917fd1fd618b3e88aa7135e71240832b
ramseyjiang <ramseyjiang@gmail.com>
2024-10-21 22:41:29 +1000

    update

--- interviews/str/longestsubstrnorepeat/impl.go
+++ interviews/str/longestsubstrnorepeat/impl.go
@@ -1,14 +1,21 @@
 package longestsubstrnorepeat
 
 func findLongestSubstr(s string) int {
-	charIndex := make(map[byte]int)
 	maxLength, start := 0, 0
+	charIndex := make(map[byte]int)
 
 	for i := 0; i < len(s); i++ {
+		// After a repeating character is found, the if condition will work.
 		if idx, found := charIndex[s[i]]; found && idx >= start {
 			start = idx + 1
 		}
+
+		// If no repeating characters found, element always fills in charIndex.
+		// If the repeating found, the repeating index will refresh the old index.
 		charIndex[s[i]] = i
+
+		// Compare substrings and get the longest substring
+		// Because i starts from 0, it should use i-start+1, not use i-start
 		maxLength = max(maxLength, i-start+1)
 	}
 
