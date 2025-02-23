a0a6caecafd8a0a870dfcb2f40977d03fbe37086
ramseyjiang <ramseyjiang@gmail.com>
2025-02-23 22:15:04 +1000

    update random num

--- interviews/awsinterview/randnum/random.go
+++ interviews/awsinterview/randnum/random.go
@@ -3,11 +3,25 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	expression := generateExpression()
+	fmt.Println(expression)
+}
+
+func generateExpression() string {
 	operators := []rune{'+', '-', '*', '/'}
 	op := operators[rand.Intn(len(operators))]
-	expression := fmt.Sprintf("%d %c %d", rand.Intn(9), op, rand.Intn(9))
-	fmt.Println(expression)
+	firstOperand := rand.Intn(9) + 1
+
+	var secondOperand int
+	if op == '/' {
+		secondOperand = rand.Intn(9) + 1 // confirm the second operand is not zero
+	} else {
+		secondOperand = rand.Intn(9)
+	}
+	return fmt.Sprintf("%d %c %d", firstOperand, op, secondOperand)
 }
