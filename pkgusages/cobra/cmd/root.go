917837695105c2f8636d21ff067bf693f19e8436
ramseyjiang <ramseyjiang@gmail.com>
2025-03-06 21:35:13 +1000

    add go jwt example

---
+++ pkgusages/cobra/cmd/root.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "myapp",
+	Short: "A demo CLI application",
+	Long:  `This is a sample CLI application built with Cobra`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+// func init() {
+// 	// Here you will define your flags and configuration settings.
+// 	// Cobra supports persistent flags, which, if defined here,
+// 	// will be global for your application.
+//
+// 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
+//
+// 	// Cobra also supports local flags, which will only run
+// 	// when this action is called directly.
+// 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+// }
---
+++ pkgusages/cobra/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/ramseyjiang/go_mid_to_senior/pkgusages/cobra/cmd"
+
+func main() {
+	cmd.Execute()
+}
